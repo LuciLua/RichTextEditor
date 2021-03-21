0148d80a01d18eb9e280c66741ba508adef3c664
LuciLua <luci.lua81@gmail.com>
2021-03-21 19:49:46 -0300

    Rich Text

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var sp2 = document.getElementById('sp2');
 var sp3 = document.getElementById('sp3');
 var sp4 = document.getElementById('sp4');
 var sp5 = document.getElementById('sp5');
+var sp6 = document.getElementById('sp6');
+
+var previewButton = document.getElementById('PreviewButton')
+var preview = document.getElementById('preview')
+var pre = document.getElementById('pre')
+var attA = document.getElementById('attA')
 
 
 let output = document.getElementById('output');
@@ -22,7 +28,6 @@ for (let btn of buttons) {
 }
 
 
-
 function mud(){
     var element = document.getElementById("editor")
     element.classList.toggle("edit");
@@ -36,8 +41,20 @@ function mud(){
     sp3.classList.toggle('menu-fff')
     sp4.classList.toggle('menu-fff')
     sp5.classList.toggle('menu-fff')
+    sp6.classList.toggle('menu-fff')
+    preview.classList.toggle('preview-toggle')
+
+
+
+
 };
 
-function toLimit(string = ""){
-    string.value = string.value.substring(0,10);
-}
+function bt(){
+    preview.innerText = output.innerHTML;
+    pre.classList.toggle('pre-dis')
+    previewButton.classList.toggle('previewButtonDepois')
+};
+
+function att(){
+    preview.innerText = output.innerHTML;
+};
